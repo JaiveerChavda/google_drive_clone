0afb4910239541e0dee18eb6b8122b3bb7532ee1
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-06-20 18:34:25 +0530

    new upload dropdown component on sidebar navigation

--- resources/js/Components/Navigation.vue
+++ resources/js/Components/Navigation.vue
@@ -2,6 +2,8 @@
 import ApplicationLogo from './ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
 import NavLink from './NavLink.vue';
+import NewUploadDropdown from '@/Components/app/NewUploadDropdown.vue';
+
 </script>
 
 <template>
@@ -10,21 +12,24 @@ import NavLink from './NavLink.vue';
         <!-- Logo -->
         <div class="h-[80px] px-3 flex items-center gap-3">
             <Link href="/dashboard" class="flex">
-                <ApplicationLogo
-                    class="block h-9 w-auto fill-current text-gray-800"
-                />
+            <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
             </Link>
             <!-- App name -->
             <p class="font-semibold uppercase tracking-widest">Drive</p>
         </div>
+
         <div class="px-3">
-            <!-- Todo CreateNewUploadDropdown -->
+
+            <!-- Upload Button (file and folder upload) -->
+            <NewUploadDropdown />
+
+            <!-- Other Links -->
             <div>
-            <NavLink href="/dashboard" :active="$page.url == '/dashboard'">My Files</NavLink>
-            <NavLink href="/">Shared with me</NavLink>
-            <NavLink href="/">Shared by me</NavLink>
-            <NavLink href="/">Trash</NavLink>
-        </div>
+                <NavLink href="/dashboard" :active="$page.url == '/dashboard'">My Files</NavLink>
+                <NavLink href="/">Shared with me</NavLink>
+                <NavLink href="/">Shared by me</NavLink>
+                <NavLink href="/">Trash</NavLink>
+            </div>
         </div>
     </nav>
 </template>
---
+++ resources/js/Components/app/NewUploadDropdown.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { DocumentPlusIcon, FolderPlusIcon, PlusIcon } from '@heroicons/vue/20/solid';
+import Dropdown from '@/Components/Dropdown.vue';
+</script>
+<template>
+    <Dropdown align="left">
+        <!-- upload button trigger -->
+        <template #trigger>
+            <button
+                class="mb-4 flex items-center px-4 py-4 bg-white border border-gray-300 rounded-xl shadow-md hover:bg-indigo-100 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-300">
+                <PlusIcon class="mr-2 text-gray-600" style="width: 30px;" />
+                <p class="text-sm">New</p>
+            </button>
+        </template>
+
+        <!-- upload dropdown content -->
+        <template #content>
+            <div class="px-4 flex flex-col gap-2 py-2">
+                <a href="/" class="flex items-center gap-2 text-sm py-2 hover:bg-gray-200 px-4 rounded-md">
+                    <DocumentPlusIcon class="h-5 w-5 text-gray-600" />
+                    <span>File Upload</span>
+                </a>
+                <a href="/" class="flex items-center gap-2 text-sm py-2 hover:bg-gray-200 px-4 rounded-md">
+                    <FolderPlusIcon class="h-5 w-5 text-gray-600" />
+                    <span>Folder Upload</span>
+                </a>
+
+            </div>
+
+
+        </template>
+    </Dropdown>
+</template>
