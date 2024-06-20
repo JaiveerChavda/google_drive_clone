76eee059bee0a34d98143119e6081d7c9c96ec4a
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-06-20 06:20:19 +0000

    creating database schemas

---
+++ database/migrations/2024_06_20_052127_create_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('path', 255)->nullable();
+            $table->nestedSet();
+            $table->boolean('is_folder')->default(0);
+            $table->string('mime')->nullable();
+            $table->integer('size')->nullable();
+            $table->timestamps();
+            $table->foreignIdFor(User::class, 'created_by');
+            $table->foreignIdFor(User::class, 'updated_by');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
