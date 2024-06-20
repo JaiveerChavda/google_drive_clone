29a0eca097b4938d4eb63cbeceef348322e0565a
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-06-20 15:04:10 +0530

    Add Side bar Navigation

---
+++ resources/js/Components/Navigation.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import ApplicationLogo from './ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+import NavLink from './NavLink.vue';
+</script>
+
+<template>
+    <nav class="min-w-[200px]">
+
+        <!-- Logo -->
+        <div class="h-[80px] px-3 flex items-center gap-3">
+            <Link href="/dashboard" class="flex">
+                <ApplicationLogo
+                    class="block h-9 w-auto fill-current text-gray-800"
+                />
+            </Link>
+            <!-- App name -->
+            <p class="font-semibold uppercase tracking-widest">Drive</p>
+        </div>
+        <div class="px-3">
+            <!-- Todo CreateNewUploadDropdown -->
+            <div class="px-3">
+            <NavLink href="/">My Files</NavLink>
+            <NavLink href="/">Shared with me</NavLink>
+            <NavLink href="/">Shared by me</NavLink>
+            <NavLink href="/">Trash</NavLink>
+        </div>
+        </div>
+    </nav>
+</template>
